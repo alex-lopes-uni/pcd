544e400ba31d26cf4b12d5d11db29c5d239d2d39
alex-lopes-cap <alex-victoria.lopes@capgemini.com>
2024-12-02 14:39:01 +0000

    changed main to IscTorrents

---
+++ src/IscTorrents.java
@@ -0,0 +1,18 @@
+import app.GUI;
+import app.Node;
+
+public class IscTorrents {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("please input arguments.");
+        }
+        int port = Integer.parseInt(args[0]);
+        String rep = args[1];
+
+        Node node = new Node(rep, port);
+        GUI gui = new GUI(node);
+        gui.setFileList(node.getRepositoryFiles()); //TODO REMOVE
+        gui.openGUI();
+        node.runServer();
+    }
+}
